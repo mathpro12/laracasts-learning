096e3a99ebea488bab3b59144560b5f6c96093dc
= <=>
2017-06-01 08:48:38 -0300

    add laravel

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+
+class TasksController extends Controller
+{
+    public function index()
+    {
+    	$tasks = Task::all();
+
+    	return view('welcome')->with(compact('tasks'));
+    }
+
+    public function show(Task $task)
+    {
+    	return view('zoom', compact('task'));
+    }
+}
